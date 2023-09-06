bfc9684c6a14e07ed373428cc171735f731f60eb
Siddhant <me@siddhantgupta.in>
2023-09-06 17:58:58 +0530

    Added Temp Static Data

---
+++ src/Data/Data.js
@@ -0,0 +1,36 @@
+export const SpanData = [
+    {
+        title:"PSPS",
+        color:{
+            backGround:"linear-gradient(180deg ,#bb67ff 0%, #c484f3 100% )",
+            boxShadow:"0px 10px 20px 0px #e0c6f5",
+            },
+        Percentage:94,
+        TotalFT:370,
+        PresentFT:340,
+        LeaveFT:30,
+    },
+    {
+        title:"BH",
+        color:{
+            backGround:"linear-gradient(180deg ,#bb67ff 0%, #c484f3 100% )",
+            boxShadow:"0px 10px 20px 0px #e0c6f5",
+            },
+        Percentage:87,
+        TotalFT:54,
+        PresentFT:48,
+        LeaveFT:6,
+    },
+    {
+        title:"Cred",
+        color:{
+            backGround:"linear-gradient(180deg ,#bb67ff 0%, #c484f3 100% )",
+            boxShadow:"0px 10px 20px 0px #e0c6f5",
+            },
+        Percentage:100,
+        TotalFT:34,
+        PresentFT:34,
+        LeaveFT:0,
+    }
+
+]
