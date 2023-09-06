fa41a1b88e4b9e46e4d9459dc707b5c31d2fc8fa
Siddhant <me@siddhantgupta.in>
2023-09-06 18:00:08 +0530

    Added the layout around Git

--- src/Components/SpanData/SpanData.js
+++ src/Components/SpanData/SpanData.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./SpanData.css";
+import Cards from '../Snips/Cards/Cards.jsx';
+
 const SpanData =()=>{
     return(
         <div className="DisplayDiv">
              <h2>Span Data</h2>
-        </div>
-        
+             <Cards />
+        </div> 
     );
 }
 export default SpanData;
