aa7dcea2ffb0d5865d83882bf15c864254300006
Siddhant <me@siddhantgupta.in>
2023-09-10 05:02:30 +0530

    Home Text Uopdate

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,8 +2,24 @@ import React from "react";
 import './Home.css';
 const Home = () =>{
     return(
+        // "Your Data, Our Canvas"
+        // Draft 1:
+        // excels at turning complex data into clear, impactful visualizations. Our innovative approach  transform raw information into actionable insights, enhance decision-making, and optimize business processes, helping you achieve efficiency, transparency, and sustainable growth.
         <>
-            <h1>Home</h1>
+            <div classname='homeContainer'>
+                <div classname='homeSummary'>
+                <div classname='homeCompanyName'>
+                    Management Information Systems <span>Visualization</span>
+                </div>
+                <div classname='homeCompanyTagline'> 
+                    Your Data, <span>Our Canvas</span>
+                </div>
+                <div classname='homeCompanySummary'>
+                    Excels at turning complex data into clear, impactful visualizations. Our innovative approach  transform raw information into actionable insights, enhance decision-making, and optimize business processes, helping you achieve efficiency, transparency, and sustainable growth.
+                </div>
+                </div>
+                <div classname='homeVisual'></div>
+            </div>
         </>
     );
 
