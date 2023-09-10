50d21d026b852240c66f7fd7fc84727e2094d6c7
Siddhant <me@siddhantgupta.in>
2023-09-11 03:36:42 +0530

    Chart Added for the Percentage

--- src/Components/TotalAttn/TotalAttn.jsx
+++ src/Components/TotalAttn/TotalAttn.jsx
@@ -1,7 +1,33 @@
 import './TotalAttn.css';
+
+// Chart 
+import {
+    CircularProgressbar ,buildStyles } from "react-circular-progressbar";
+import 'react-circular-progressbar/dist/styles.css';
+
+
 const TotalAttn = () =>{
     return(
-        <h1>TotalAttn</h1>
+       <>
+            <div className='totalAttnContainer'>
+                <div className='totalAttnChart'>
+                <CircularProgressbar
+                    value={88}
+                    text={`${88}%`}
+                    circleRatio={0.75}
+                    strokeWidth={9}s
+                    styles={buildStyles({
+                      textColor: "#75e091",
+                      pathColor: "#75e091", 
+                      trailColor: "#f7afb2",
+                      rotation: 1 / 2 + 1 / 8,
+                    })}
+                />
+                </div>
+                <div className='totalAttnData'>Data</div>
+            </div>
+            
+       </>
     );
 }
 export default TotalAttn;
