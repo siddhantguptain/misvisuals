7de30fdbc96d009cd3c36b7689dca8c69e019e22
Siddhant <me@siddhantgupta.in>
2023-09-11 02:23:26 +0530

    Total Attendane Snip/Template  Setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
-import Home from './Components/Home/Home.jsx';
+// import Home from './Components/Home/Home.jsx';
 
-import SideBar from "./Components/Sidebar/SideBar.js";
-import ParseExcel from './Components/ParseExcel/ParseExcel.js';
-import SpanData from './Components/SpanData/SpanData.js';
+// import SideBar from "./Components/Sidebar/SideBar.js";
+// import ParseExcel from './Components/ParseExcel/ParseExcel.js';
+// import SpanData from './Components/SpanData/SpanData.js';
+
+import TotalAttn from './Components/TotalAttn/TotalAttn.jsx';
 
 
 function App() {
   return (
     <div className="container">
-      <Home />
+      <TotalAttn />
+      {/* <Home /> */}
       {/* <SideBar />
      <div className="mainContainer">
      <ParseExcel />
---
+++ src/Components/TotalAttn/TotalAttn.jsx
@@ -0,0 +1,7 @@
+import './TotalAttn.css';
+const TotalAttn = () =>{
+    return(
+        <h1>TotalAttn</h1>
+    );
+}
+export default TotalAttn;
