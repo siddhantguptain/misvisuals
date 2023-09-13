06d3ed46f3e25d416eae013b74b94e9279b85339
Siddhant <me@siddhantgupta.in>
2023-09-13 10:07:23 +0530

    Added the Grouped Chart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import Home from './Components/Home/Home.jsx';
+// import Home from './Components/Home/Home.jsx';
 
 import SideBar from "./Components/Sidebar/SideBar.js";
-import ParseExcel from './Components/ParseExcel/ParseExcel.js';
-import SpanData from './Components/SpanData/SpanData.js';
+// import ParseExcel from './Components/ParseExcel/ParseExcel.js';
+// import SpanData from './Components/SpanData/SpanData.js';
 
 import TotalAttn from './Components/TotalAttn/TotalAttn.jsx';
 
@@ -12,9 +12,10 @@ import TotalAttn from './Components/TotalAttn/TotalAttn.jsx';
 function App() {
   return (
     <div className="container">
+        {/* <SideBar />  */}
       <TotalAttn />
-      {/* <SideBar />
-      <Home /> */}
+   
+      {/* <Home />  */}
       
       
      {/* <div className="mainContainer">
--- src/Components/GroupAttn/GroupAttn.jsx
+++ src/Components/GroupAttn/GroupAttn.jsx
@@ -1,5 +1,6 @@
+import React, {useState} from 'react';
 import './GroupAttn.css';
-
+import Chart from "react-apexcharts";
 // Chart 
 // import {
 //     CircularProgressbar ,buildStyles ,CircularProgressbarWithChildren } from "react-circular-progressbar";
@@ -7,6 +8,66 @@ import './GroupAttn.css';
 
 
 const GroupAttn =()=>{
+//  chart Dynamic data 
+ const options = {
+    series: [76, 100],
+          chart: {
+          height: 390,
+          type: 'radialBar',
+        },
+        plotOptions: {
+          radialBar: {
+            offsetY: 0,
+            startAngle: 0,
+            endAngle: 270,
+            hollow: {
+              margin: 20,
+              size: '30%',
+              background: 'transparent',
+              image: undefined,
+            },
+            dataLabels: {
+              name: {
+                show: false,
+              },
+              value: {
+                show: false,
+              }
+            }
+          }
+        },
+        colors: ['#3d861d','#76dd49' ],
+        labels: ['WFO', 'WFH'],
+        legend: {
+          show: true,
+          floating: true,
+          fontSize: '12px',
+          position: 'left',
+          offsetX: 1,
+          offsetY: 15,
+          labels: {
+            useSeriesColors: true,
+          },
+          markers: {
+            size: 0
+          },
+        //  7 Need to add the percentage sign here
+          formatter: function(seriesName, opts) {
+            return seriesName + ":  " + opts.w.globals.series[opts.seriesIndex]
+          },
+          itemMargin: {
+            vertical: 1
+          }
+        },
+        // responsive: [{
+        //   breakpoint: 480,
+        //   options: {
+        //     legend: {
+        //         show: false
+        //     }
+        //   }
+        // }]
+            }
     return(
         <>
             <div className='groupAttnContainer'>
@@ -17,7 +78,7 @@ const GroupAttn =()=>{
 
                 </div>
                 <div className='groupAttnContainerChart'>
-                        Chart
+                         <Chart options={options} series={options.series} type="radialBar" width={300} height={220} />
                 </div>
 
             </div>
