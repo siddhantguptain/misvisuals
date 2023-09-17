0e157073bdcf24246f08037e9f105d34d82443c1
Siddhant <me@siddhantgupta.in>
2023-09-17 21:33:40 +0530

    Added the Chart template

--- src/Components/ProductivityTracker/ProductivityTracker.jsx
+++ src/Components/ProductivityTracker/ProductivityTracker.jsx
@@ -1,8 +1,99 @@
 import './ProductivityTracker';
 
+import Chart from "react-apexcharts";
+
 const ProductivityTracker = () =>{
+
+    const  series = [{
+        name: 'Inflation',
+        data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2]
+      }]
+
+    const options = {
+        chart: {
+        height: 350,
+        type: 'bar',
+      },
+      plotOptions: {
+        bar: {
+          borderRadius: 20,
+          dataLabels: {
+            position: 'top', // top, center, bottom
+          },
+        }
+      },
+      dataLabels: {
+        enabled: true,
+        formatter: function (val) {
+          return val + "%";
+        },
+        offsetY: -20,
+        style: {
+          fontSize: '12px',
+          colors: ["#304758"]
+        }
+      },
+      
+      xaxis: {
+        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        position: 'bottom', 
+        axisBorder: {
+          show: false
+        },
+        axisTicks: {
+          show: false
+        },
+        crosshairs: {
+          fill: {
+            type: 'gradient',
+            gradient: {
+              colorFrom: '#D8E3F0',
+              colorTo: '#BED1E6',
+              stops: [0, 100],
+              opacityFrom: 0.4,
+              opacityTo: 0.5,
+            }
+          }
+        },
+        tooltip: {
+          enabled: true,
+        }
+      },
+      yaxis: {
+        axisBorder: {
+          show: false
+        },
+        axisTicks: {
+          show: false,
+        },
+        labels: {
+          show: false,
+          formatter: function (val) {
+            return val + "%";
+          }
+        }
+      
+      },
+      title: {
+        text: 'Monthly Inflation in Argentina, 2002',
+        floating: true,
+        offsetY: 0,
+        align: 'center',
+        style: {
+          color: '#444'
+        }
+      }
+    }
+
     return(
-        <h1>ProductivityTracker</h1>
+        <>
+            <div className='productivityTrackerContainer'>
+                <div className='productivityTrackerSub1'></div>
+                <div className='productivityTrackerSub2'>
+                    <Chart options={options} series={series} type="bar" width={800} height={400} />   
+                </div>
+            </div>
+        </>
     );
 }
 
