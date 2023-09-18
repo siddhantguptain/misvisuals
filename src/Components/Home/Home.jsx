7b973b9449b831bcbb0d0c243612f03b4f668e73
Siddhant <me@siddhantgupta.in>
2023-09-18 08:55:23 +0530

    Home Img setup

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+
 import './Home.css';
+import HomeImg from './HomeImg/HomeImg.jsx';
 const Home = () =>{
     return(
         // "Your Data, Our Canvas"
@@ -8,21 +10,22 @@ const Home = () =>{
         <>
             <div className='homeContainer'>
                 <div className='homeSummary'>
-                <div className='homeCompanyName'>
-                    Management Information Systems <span className='homeCompanyNameEdit'>Visualization</span>
-                </div>
-                <div className='homeCompanyTagline'> 
-                    Your Data, <div className='homeCompanyTaglineEdit'>Our Canvas.</div>
-                </div>
-                <div className='homeCompanySummary'>
-                    Excels at turning complex data into clear, impactful visualizations. Our innovative approach  transform raw information into actionable insights, enhance decision-making, and optimize business processes, helping you achieve efficiency, transparency, and sustainable growth.
-                </div>
-                <div className='homeButton'>
-                    Expore Template  ->
-                </div>
-                                    
-                </div>
-                <div className='homeVisual'>+IMG..+</div>
+                    <div className='homeCompanyName'>
+                        Management Information Systems <span    className='homeCompanyNameEdit'>Visualization</span>
+                    </div>
+                        <div className='homeCompanyTagline'> 
+                            Your Data, <div         className='homeCompanyTaglineEdit'>Our Canvas.      </div>
+                     </div>
+                        <div className='homeCompanySummary'>
+                        Excels at turning complex data into clear, impactful visualizations. Our innovative approach  transform raw information into actionable insights, enhance decision-making, and optimize business processes, helping you achieve efficiency, transparency, and sustainable growth.
+                        </div>
+                        <div className='homeButton'>
+                             Expore Template  ->
+                        </div>   
+                 </div>
+                     <div className='homeVisual'>
+                            <HomeImg />
+                     </div>
             </div>
         </>
     );
---
+++ src/Components/Home/HomeImg/HomeImg.jsx
@@ -0,0 +1,10 @@
+import './HomeImg.css';
+
+const HomeImg = () =>{
+    return(
+        <>
+            <h1>HomeImg</h1>
+        </>
+    );
+} 
+export default HomeImg;
